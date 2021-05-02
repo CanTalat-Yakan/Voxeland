cf88be27a5f059e757dc91f597a1c9d355cb680a
Can <can.talat.yakan@gmail.com>
2021-05-02 19:04:36 +0200

    Updated TextureOffsetting for the clouds to work for x and z axis.

--- Voxeland/Assets/Game/Scripts/Miscellaneous/TimeOffset.cs
+++ Voxeland/Assets/Game/Scripts/Miscellaneous/TimeOffset.cs
@@ -2,17 +2,12 @@ using UnityEngine;
 
 public class TimeOffset : MonoBehaviour
 {
-    [SerializeField] float scrollSpeed = 0.01f;
-    Renderer m_rend;
+    [SerializeField] Vector2 m_scrollSpeed = new Vector2(1, 1);
+    [SerializeField] Renderer m_rend;
 
-    void Start()
-    {
-        m_rend = GetComponent<Renderer>();
-    }
 
     void Update()
     {
-        float offset = Time.time * scrollSpeed;
-        m_rend.material.mainTextureOffset = (new Vector2(offset, 0));
+        m_rend.material.mainTextureOffset = (m_scrollSpeed * Time.time) * 0.001f;
     }
 }
