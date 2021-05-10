08e5b211b0d19c1b7606e44475e179ab1d18ca5e
Can <can.talat.yakan@gmail.com>
2021-05-10 20:39:25 +0200

    Updated the LocalPlayerOnly Script

--- Voxeland/Assets/Game/Scripts/Miscellaneous/LocalPlayerOnly.cs
+++ Voxeland/Assets/Game/Scripts/Miscellaneous/LocalPlayerOnly.cs
@@ -2,16 +2,26 @@ using UnityEngine;
 
 public class LocalPlayerOnly : Mirror.NetworkBehaviour
 {
+    [SerializeField] private Camera m_camera;
+
     void Start()
     {
         // Network Move on LocalPlayer only
         if (isLocalPlayer)
+        {
+            if (GameManager.Instance)
+            {
+                GameManager.Instance.m_MainCamera = m_camera;
+                GameManager.Instance.m_Player = gameObject;
+            }
             return;
+        }
 
         MonoBehaviour[] comps = GetComponents<MonoBehaviour>();
         foreach (MonoBehaviour c in comps)
             c.enabled = false;
-
         // GetComponent<...>().enabled = true;
+
+        m_camera.enabled = false;
     }
 }
