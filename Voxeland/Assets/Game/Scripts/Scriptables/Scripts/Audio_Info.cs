19d4b0a45b1f2b7170785a5ff025d47cac87d033
Can <can.talat.yakan@gmail.com>
2021-05-20 09:55:07 +0200

    Added a Scriptable Object for Audio Information

---
+++ Voxeland/Assets/Game/Scripts/Scriptables/Scripts/Audio_Info.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+[CreateAssetMenu(menuName = "Scriptables/Audio Info", fileName = "Audio Info", order = 1)]
+public class Audio_Info : ScriptableObject
+{
+    [Header("Music")]
+    public AudioClip MainMusic;
+    [Header("Menu")]
+    public AudioClip ButtonSelect;
+    public AudioClip ButtonMove;
+    public AudioClip Joined;
+
+    [Header("Gameplay")]
+    public AudioClip Attack;
+    public AudioClip Fall;
+    [Tooltip("0 = Dirt, 1 = Stone, 2 = Glass")]
+    public AudioClip[] BlockAdded = new AudioClip[3];
+    [Tooltip("0 = Dirt, 1 = Stone, 2 = Glass")]
+    public AudioClip[] BlockRemoved = new AudioClip[3];
+}
+public enum BlockSoundTypes
+{
+    DIRT,
+    STONE,
+    GLASS
+}
