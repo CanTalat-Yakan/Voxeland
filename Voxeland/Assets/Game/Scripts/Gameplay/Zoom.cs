3d0f6438fde63f26a8ae4ac8813e07db6e4f0573
Can <can.talat.yakan@gmail.com>
2021-05-20 09:53:11 +0200

    Updated Zoom to work with GameManagers lock and smoothing when zoomed in

--- Voxeland/Assets/Game/Scripts/Gameplay/Zoom.cs
+++ Voxeland/Assets/Game/Scripts/Gameplay/Zoom.cs
@@ -6,20 +6,29 @@ public class Zoom : MonoBehaviour
 {
     Camera m_camera;
     [SerializeField] int m_zoomFOV = 30;
+    [SerializeField] KeyCode m_key = KeyCode.C;
+    ECM.Components.MouseLook m_mouseLook;
 
     void Start()
     {
+        m_mouseLook = GetComponent<ECM.Components.MouseLook>();
     }
 
     void LateUpdate()
     {
+        m_mouseLook.smooth = false;
+
         if (m_camera is null)
         {
             if (GameManager.Instance)
                 if (GameManager.Instance.m_MainCamera)
                     m_camera = GameManager.Instance.m_MainCamera;
         }
-        else if (Input.GetKey(KeyCode.LeftAlt))
+        else if (Input.GetKey(m_key) && !GameManager.Instance.LOCKED)
+        {
             m_camera.fieldOfView = m_zoomFOV;
+            m_mouseLook.smooth = true;
+        }
+
     }
 }
