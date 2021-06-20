24d5a35bdf4d7a8aedbfac8ff6a1a6af8ad780a0
Can <can.talat.yakan@gmail.com>
2021-06-20 21:24:01 +0200

    Added FollowMainCamera to Sun and Clouds; Changed Directional Light Depth to 2.5 from 1

---
+++ Voxeland/Assets/Game/Scripts/Miscellaneous/FollowMainCamera.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMainCamera : MonoBehaviour
+{
+    Vector3 m_initialPos;
+
+    void Start() { m_initialPos = transform.position; }
+    void Update()
+    {
+        if (GameManager.Instance.m_MainCamera)
+            transform.position = GameManager.Instance.m_MainCamera.transform.position + m_initialPos;
+
+    }
+}
