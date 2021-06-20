cf51ad6238dc9b3ade63b32056fd199e619b9b1c
Can <can.talat.yakan@gmail.com>
2021-06-20 22:09:58 +0200

    Added FooterLink Opener to Menu

---
+++ Voxeland/Assets/Game/Scripts/Miscellaneous/FooterLinkOpener.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FooterLinkOpener : MonoBehaviour
+{
+    public void OpenTermsOfUse() { Application.OpenURL("http://unity3d.com/"); }
+    public void OpenPrivacyPolicy() { Application.OpenURL("http://unity3d.com/"); }
+}
