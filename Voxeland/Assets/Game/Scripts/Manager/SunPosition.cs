2e4cbc12821b715a1946466ef6c665b4588a12b8
Can <can.talat.yakan@gmail.com>
2021-06-21 16:29:40 +0200

    Moved Scripts

---
+++ Voxeland/Assets/Game/Scripts/Manager/SunPosition.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunPosition : MonoBehaviour
+{
+    [SerializeField] ParticleSystem m_sky;
+    [SerializeField] GameObject m_sun;
+    [SerializeField] Light m_directionalLight;
+    [Range(0, 1440)]
+    [SerializeField] double m_dayTime = 846;
+    bool m_day = false;
+
+    void Update()
+    {
+        if(!GameManager.Instance) return;
+        if(!GameManager.Instance.m_MainCamera) return;
+        if(GameManager.Instance.LOCKED) return;
+
+        m_sun.transform.position = m_directionalLight.transform.forward * -500;
+
+        m_dayTime = System.DateTime.Now.TimeOfDay.TotalMinutes;
+        m_directionalLight.transform.rotation = Quaternion.Euler((float)(m_dayTime - 360) * 0.25f, -30, 0);
+
+        SetIntensityOfSun();
+
+        if (m_day)
+            m_sky.Stop();
+        else
+            m_sky.Play();
+    }
+    void SetIntensityOfSun()
+    {
+        float a = Vector3.Dot(Vector3.up, m_sun.transform.position - GameManager.Instance.m_MainCamera.transform.position);
+
+        RenderSettings.ambientLight = Color.white * GameManager.Instance.Map(a, -50, 20, 0.65f, 1f);
+        m_directionalLight.intensity = GameManager.Instance.Map(a, -20, 20, 0, 1.3f);
+
+        m_day = a > 0;
+    }
+}
