0d8141adbbe88638c46c46e976bca80a7a930565
Can <can.talat.yakan@gmail.com>
2021-06-22 19:41:29 +0200

    Added AnimationController

---
+++ Voxeland/Assets/Game/Scripts/Network/AnimationController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : Mirror.NetworkBehaviour
+{
+    [SerializeField] Animator anim;
+    public bool run = false;
+
+    void Start()
+    {
+        run = false;
+    }
+
+    Vector3 tmpPos;
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+        if (GameManager.Instance.LOCKED) return;
+
+        run = Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0;
+        if (transform.position == tmpPos)
+            run = false;
+        tmpPos = transform.position;
+
+        anim.SetBool("Run", run);
+    }
+}
