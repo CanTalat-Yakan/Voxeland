dc5305370bf40b266f19e8cfdb64ede1d09569ae
Can <can.talat.yakan@gmail.com>
2021-06-28 20:22:07 +0200

    Now sounds of footsteps, jumps and falls

--- Voxeland/Assets/Game/Scripts/Network/AnimationController.cs
+++ Voxeland/Assets/Game/Scripts/Network/AnimationController.cs
@@ -5,41 +5,67 @@ using UnityEngine;
 
 public class AnimationController : Mirror.NetworkBehaviour
 {
+    [SerializeField] ECM.Controllers.BaseFirstPersonController controller;
+    [SerializeField] Rigidbody rb;
     [SerializeField] NetworkAnimator anim;
     public bool run = false;
+    public bool walk = false;
+    AudioSource runSound;
 
-    void Start()
-    {
-        run = false;
-    }
+    void Start() { run = false; runSound = gameObject.AddComponent<AudioSource>(); rb = GetComponent<Rigidbody>(); }
 
     Vector3 tmpPos;
+    bool tmpGrounded;
+    bool tmpFalling;
+    float tmpVelocity;
     void Update()
     {
         // if (!isLocalPlayer) return;
         if (GameManager.Instance.LOCKED) return;
 
-        run = Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0;
-        if (transform.position == tmpPos)
-            run = false;
+        run = controller.run;
+        walk = !run && Input.GetAxisRaw("Horizontal") + Input.GetAxisRaw("Vertical") != 0;
+        if (transform.position == tmpPos) run = false;
         tmpPos = transform.position;
 
         anim.animator.SetBool("Run", run);
-        // RpcOnAnim();
-        // CmdAnim();
+        anim.animator.SetBool("Walk", walk);
+        anim.animator.SetBool("Crouch", controller.isCrouching);
+
+        if ((tmpGrounded != controller.isGrounded) && tmpVelocity < -10)
+            AudioManager.Instance.Play(
+                AudioManager.Instance.m_AudioInfo.Fall[
+                    tmpVelocity < -20 
+                    ? 1 
+                    : 0], 
+                AudioManager.Instance.m_AudioMixer);
+                
+        if ((!controller.isJumping && (run || walk) && !runSound.isPlaying)
+             || tmpGrounded != controller.isGrounded)
+            AudioManager.Play(
+                runSound,
+                AudioManager.PlayRandomFromList(ref AudioManager.Instance.m_AudioInfo.FootSteps[0].clips),
+                AudioManager.Instance.m_AudioMixer,
+                false,
+                0.25f,
+                controller.isCrouching
+                    ? 0.5f
+                    : controller.run
+                        ? 1.3f
+                        : 1);
+
+        tmpGrounded = controller.isGrounded;
+        tmpFalling = controller.isFalling;
+        tmpVelocity = rb.velocity.y;
+
+        // RpcOnAnim(); CmdAnim();
     }
 
     // this is called on the server
     [Command]
-    void CmdAnim()
-    {
-        RpcOnAnim();
-    }
+    void CmdAnim() { RpcOnAnim(); }
 
     // this is called for all observers
     [ClientRpc]
-    void RpcOnAnim()
-    {
-        anim.animator.SetBool("Run", run);
-    }
+    void RpcOnAnim() { anim.animator.SetBool("Run", run); }
 }
