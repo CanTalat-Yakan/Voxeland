37c17c3f9cde3018a04bd82f8468aed604ae2216
Can <can.talat.yakan@gmail.com>
2021-07-10 22:01:04 +0200

    Added Scriptable Object of Voxel Collection and Dictionary with finished Assets

---
+++ Voxeland/Assets/Game/Scripts/Scriptables/Scripts/VoxelDictionary.cs
@@ -0,0 +1,4 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptables/Voxel Dictionary", fileName = "Voxel Dictionary", order = 2)]
+public class VoxelDictionary : ScriptableObject { public VoxelInfo[] VoxelInfo; }
---
+++ Voxeland/Assets/Game/Scripts/Scriptables/Scripts/VoxelInfo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class VoxelTexture { public int front, left, back, right, top, bottom; }
+
+[CreateAssetMenu(menuName = "Scriptables/Voxel Info", fileName = "Voxel Info", order = 3)]
+public class VoxelInfo : ScriptableObject
+{
+    [Space(10)]
+    public short ID = 0;
+    public string VoxelName = "Default";
+    public bool Transparent = false;
+    public float Durability = 1.0f;
+    [Space(10)]
+    public VoxelTexture VoxelTexture = null;
+
+    void OnValidate()
+    {
+        ID = (short)Mathf.Clamp(ID, 0, short.MaxValue);
+        Durability = Mathf.Clamp(Durability, 0, Mathf.Infinity);
+    }
+}
