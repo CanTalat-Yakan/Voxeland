30257dfec14b8057504a12a82ec17a2b1558b6a1
Can <can.talat.yakan@gmail.com>
2021-07-10 22:08:51 +0200

    Fixed AudioManager with new Parameters and now AudioMixerGroup. TODO create own channel for FX

--- Voxeland/Assets/Game/Scripts/Manager/AudioManager.cs
+++ Voxeland/Assets/Game/Scripts/Manager/AudioManager.cs
@@ -8,7 +8,7 @@ public class AudioManager : MonoBehaviour
     public static AudioManager Instance { get; private set; }
 
     [SerializeField] internal AudioInfo m_AudioInfo;
-    [SerializeField] internal AudioMixer m_AudioMixer;
+    [SerializeField] internal AudioMixerGroup m_AudioMixer;
     AudioSource m_mainMusicSource;
     float m_volumeOffset = 100, m_tmpVolume = 14;
 
@@ -65,10 +65,9 @@ public class AudioManager : MonoBehaviour
 
         return audioSource;
     }
-    internal static AudioSource Play(AudioSource _source, AudioClip _clip, AudioMixer _mixer = null, bool? _loop = null, float? _volume = null, float? _pitch = null)
+    internal static AudioSource Play(AudioSource _source, AudioClip _clip, bool? _loop = null, float? _volume = null, float? _pitch = null)
     {
         _source.clip = _clip;
-        if (_mixer) _source.outputAudioMixerGroup = _mixer.outputAudioMixerGroup;
         if (_loop != null) _source.loop = _loop.Value;
         if (_volume != null) _source.volume = _volume.Value;
         if (_pitch != null) _source.pitch = _pitch.Value;
