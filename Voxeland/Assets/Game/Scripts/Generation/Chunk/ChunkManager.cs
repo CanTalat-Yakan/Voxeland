aa90c971907745a3c155a6c6e1e58133c1cdf20c
Can <can.talat.yakan@gmail.com>
2021-07-10 22:03:22 +0200

    Chunk Manager Script

---
+++ Voxeland/Assets/Game/Scripts/Generation/Chunk/ChunkManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkManager : MonoBehaviour
+{
+    internal Chunk Chunk { get; set; }
+    internal int RenderDistance { get => 1 << Chunk.Master.RenderDistance + Chunk.LOD; }
+    internal bool Dirty { get; set; }
+
+    Camera mainCamera;
+    float tick = 0.5f;
+    float timer = 0f;
+
+    void Start() { mainCamera = GameManager.Instance.m_MainCamera; }
+    void Update()
+    {
+        if (timer < tick)
+            timer += Time.deltaTime;
+        else
+        {
+            UpdateChunk();
+            timer = 0;
+        }
+    }
+
+    void UpdateChunk()
+    {
+        if (mainCamera is null || Chunk is null || Chunk.Master is null) return;
+
+        Vector3 dir = mainCamera.transform.position - Chunk.CenteredPosition;
+        float offset = offset = Vector3.Magnitude(Vector3.Normalize(dir) * Chunk.Diameter * ((1 << Chunk.Master.RenderDistance) >> 7));
+        float dist = Vector3.Magnitude(dir);
+
+        bool inRange = (dist - offset) <= RenderDistance;
+        if (Chunk.LOD != 0) inRange &= (dist + offset) > (RenderDistance >> 1);
+
+        bool canDispose = dist > RenderDistance * Chunk.Master.DisposeFactor;
+        if (Chunk.LOD != 0) canDispose = true;
+
+        Chunk.SetVisible = inRange;
+
+        if (!Dirty)
+            if (!inRange && canDispose)
+                Chunk.Remove();
+    }
+}
