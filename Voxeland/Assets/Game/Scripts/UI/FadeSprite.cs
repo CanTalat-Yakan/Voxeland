7616d2fcd160350041e115bb0094887651075e26
Can <can.talat.yakan@gmail.com>
2021-07-10 22:00:15 +0200

    Added new Sprites and set repeat of default img

---
+++ Voxeland/Assets/Game/Scripts/UI/FadeSprite.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeSprite : MonoBehaviour
+{
+    Image img;
+    Sprite lastTexture;
+    void Start()
+    {
+        img = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (img.sprite != lastTexture)
+        {
+            StopAllCoroutines();
+            StartCoroutine(StarFade(2, 0.33f));
+        }
+        lastTexture = img.sprite;
+    }
+
+    IEnumerator StarFade(float _duration, float _fadeOutDuration)
+    {
+        img.color = Color.white;
+
+        float startTime = Time.time;
+        float endTime = startTime + _duration;
+        yield return new WaitUntil(() => Time.time > endTime);
+
+        startTime = Time.time;
+        endTime = startTime + _fadeOutDuration;
+        while (Time.time < endTime)
+        {
+            float value = (Time.time - startTime) / _fadeOutDuration;
+            img.color = new Color(1, 1, 1, Mathf.Lerp(1, 0, value));
+            yield return new WaitForEndOfFrame();
+        }
+        img.color = new Color(1, 1, 1, 0);
+
+        yield return null;
+    }
+}
