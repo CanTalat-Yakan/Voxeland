9aac235f0d50ff80e3fc62fa128627a6b3ca0003
Can <can.talat.yakan@gmail.com>
2021-07-14 16:15:19 +0200

    Added ChunkPrefab for PrefabPool for Generation that hold MeshInfo Script with references on GameObject

---
+++ Voxeland/Assets/Game/Scripts/Generation/Chunk/MeshInfo.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class MeshInfo : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    public MeshFilter Filter;
+    public MeshCollider Collider;
+    public ChunkManager Manager;
+    public Mesh Mesh;
+}
